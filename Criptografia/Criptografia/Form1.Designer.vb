aef8c1163ab8b05d88b5b9fe998d48757bec44a1
EDUM1SAEL <andyguzman4c21@gmail.com>
2022-11-21 10:35:18 -0600

    prueba 2

---
+++ Criptografia/Criptografia/Form1.Designer.vb
@@ -0,0 +1,141 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form reemplaza a Dispose para limpiar la lista de componentes.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Requerido por el Diseñador de Windows Forms
+    Private components As System.ComponentModel.IContainer
+
+    'NOTA: el Diseñador de Windows Forms necesita el siguiente procedimiento
+    'Se puede modificar usando el Diseñador de Windows Forms.  
+    'No lo modifique con el editor de código.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Form1))
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.GroupBox1 = New System.Windows.Forms.GroupBox()
+        Me.TextBoxTexto = New System.Windows.Forms.TextBox()
+        Me.ButtonCifrar = New System.Windows.Forms.Button()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.TextBoxcifrado = New System.Windows.Forms.TextBox()
+        Me.PictureBox1 = New System.Windows.Forms.PictureBox()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.GroupBox1.SuspendLayout()
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(12, 44)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(33, 13)
+        Me.Label1.TabIndex = 0
+        Me.Label1.Text = "Frase"
+        '
+        'GroupBox1
+        '
+        Me.GroupBox1.BackColor = System.Drawing.Color.FromArgb(CType(CType(255, Byte), Integer), CType(CType(224, Byte), Integer), CType(CType(192, Byte), Integer))
+        Me.GroupBox1.Controls.Add(Me.TextBoxcifrado)
+        Me.GroupBox1.Controls.Add(Me.Label2)
+        Me.GroupBox1.Location = New System.Drawing.Point(12, 110)
+        Me.GroupBox1.Name = "GroupBox1"
+        Me.GroupBox1.Size = New System.Drawing.Size(402, 114)
+        Me.GroupBox1.TabIndex = 1
+        Me.GroupBox1.TabStop = False
+        '
+        'TextBoxTexto
+        '
+        Me.TextBoxTexto.Location = New System.Drawing.Point(51, 41)
+        Me.TextBoxTexto.Name = "TextBoxTexto"
+        Me.TextBoxTexto.Size = New System.Drawing.Size(187, 20)
+        Me.TextBoxTexto.TabIndex = 1
+        '
+        'ButtonCifrar
+        '
+        Me.ButtonCifrar.BackColor = System.Drawing.Color.FromArgb(CType(CType(192, Byte), Integer), CType(CType(255, Byte), Integer), CType(CType(192, Byte), Integer))
+        Me.ButtonCifrar.Location = New System.Drawing.Point(244, 28)
+        Me.ButtonCifrar.Name = "ButtonCifrar"
+        Me.ButtonCifrar.Size = New System.Drawing.Size(73, 45)
+        Me.ButtonCifrar.TabIndex = 2
+        Me.ButtonCifrar.Text = "Cifrar"
+        Me.ButtonCifrar.UseVisualStyleBackColor = False
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(31, 49)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(69, 13)
+        Me.Label2.TabIndex = 1
+        Me.Label2.Text = "Frase Cifrada"
+        '
+        'TextBoxcifrado
+        '
+        Me.TextBoxcifrado.Location = New System.Drawing.Point(121, 46)
+        Me.TextBoxcifrado.Name = "TextBoxcifrado"
+        Me.TextBoxcifrado.Size = New System.Drawing.Size(197, 20)
+        Me.TextBoxcifrado.TabIndex = 2
+        '
+        'PictureBox1
+        '
+        Me.PictureBox1.Image = CType(resources.GetObject("PictureBox1.Image"), System.Drawing.Image)
+        Me.PictureBox1.Location = New System.Drawing.Point(436, 28)
+        Me.PictureBox1.Name = "PictureBox1"
+        Me.PictureBox1.Size = New System.Drawing.Size(227, 196)
+        Me.PictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage
+        Me.PictureBox1.TabIndex = 3
+        Me.PictureBox1.TabStop = False
+        '
+        'Button1
+        '
+        Me.Button1.BackColor = System.Drawing.Color.FromArgb(CType(CType(192, Byte), Integer), CType(CType(255, Byte), Integer), CType(CType(192, Byte), Integer))
+        Me.Button1.Location = New System.Drawing.Point(341, 28)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(73, 45)
+        Me.Button1.TabIndex = 4
+        Me.Button1.Text = "decifrar"
+        Me.Button1.UseVisualStyleBackColor = False
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackColor = System.Drawing.Color.Silver
+        Me.ClientSize = New System.Drawing.Size(683, 247)
+        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.PictureBox1)
+        Me.Controls.Add(Me.ButtonCifrar)
+        Me.Controls.Add(Me.GroupBox1)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.TextBoxTexto)
+        Me.Name = "Form1"
+        Me.Text = "Criptografia"
+        Me.GroupBox1.ResumeLayout(False)
+        Me.GroupBox1.PerformLayout()
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents Label1 As Label
+    Friend WithEvents GroupBox1 As GroupBox
+    Friend WithEvents TextBoxcifrado As TextBox
+    Friend WithEvents Label2 As Label
+    Friend WithEvents TextBoxTexto As TextBox
+    Friend WithEvents ButtonCifrar As Button
+    Friend WithEvents PictureBox1 As PictureBox
+    Friend WithEvents Button1 As Button
+End Class
---
+++ Criptografia/Criptografia/Form1.vb
@@ -0,0 +1,32 @@
+﻿Public Class Form1
+    Dim fraseOriginal, fraseEncriptada, frasedesencriptada As String
+    Dim longitud, i As Integer
+    Dim par As String
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        fraseOriginal = TextBoxTexto.Text
+        frasedesencriptada = Nothing
+        longitud = fraseOriginal.Length
+        For i = 1 To longitud Step 2
+            par = Mid(fraseOriginal, i, 2).ToString
+            frasedesencriptada &= Chr(par).ToString
+
+
+        Next
+        TextBoxcifrado.Text = frasedesencriptada
+
+
+
+    End Sub
+
+    Private Sub ButtonCifrar_Click(sender As Object, e As EventArgs) Handles ButtonCifrar.Click
+        fraseOriginal = UCase(TextBoxTexto.Text)
+        fraseEncriptada = Nothing
+
+        For Each item In fraseOriginal
+            fraseEncriptada &= Asc(item.ToString)
+        Next
+
+        TextBoxcifrado.Text = fraseEncriptada
+
+    End Sub
+End Class
